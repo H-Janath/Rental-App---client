ff72b4bbddde3cfa5e75764cf1bb29bf69483fdb
H-Janath <janathhma@gmail.com>
2025-04-19 15:24:01 +0530

    Implement protected routes and integrate dashboard sidebar navigation

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -2,23 +2,48 @@
 import Navbar from '@/components/Navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
 import { useGetAuthUserQuery } from '@/state/api'
-import React from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
-const layout = ({children}:{children:React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
 
-  const {data: authUser} = useGetAuthUserQuery();
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const pathName = usePathname();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
-  console.log(authUser)
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathName.startsWith("/tenants")) ||
+        (userRole === "manager" && pathName=== "/")
+      ) {
+        router.push(
+          "/managers/properties",{ scroll: false }
+        );
+      } else {
+        setIsLoading(false)
+      }
+    }
+  }, [authUser, router, pathName]);
+
+  if (authLoading || isLoading) return <>Loading</>;
+
   return (
     <div className='h-full w-full'>
-        <Navbar/>
-        <main className={`h-full flex w-full flex-col`}
-          style={{paddingTop: `${NAVBAR_HEIGHT}px`}}
-        >
-            {children}
-        </main>
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
     </div>
   )
 }
 
 export default layout
+
+function useState(arg0: boolean): [any, any] {
+  throw new Error('Function not implemented.')
+}
